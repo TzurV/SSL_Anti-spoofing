d1878c9c1c04024093d2660fa6c85fb30ff59d95
Tzur Vaich <tzurvaich@gmail.com>
2023-09-18 13:32:39 +0100

    add DF test command line examples - not operatives
    Commented out,  a complete environment needs to be created similar to the LA that extracts part of the files.

--- run_LA.sh
+++ run_LA.sh
@@ -23,8 +23,10 @@ mkdir -p $resultsPath
 cp /app/ASVspoof2021_LA_eval/keys/LA/ASV/trial_metadata.txt $resultsPath
 cp $evalFile $resultsPath
 
-CUDA_VISIBLE_DEVICES=0 python main_SSL_LA.py --track=LA --is_eval --eval --model_path=/app/model/LA_model.pth --eval_output=$resultsPath/eval_CM_scores_file_SSL_LA.txt --database_path /app/ --protocols_path shorteval/
-
 for ID in `awk '{ sub("\r$", ""); print }' $evalFile`; do grep $ID /app/ASVspoof2021_LA_eval/keys/LA/ASV/ASVTorch_Kaldi/Original_score.txt | head -n 1; done > /app/ASVspoof2021_LA_eval/keys/LA/ASV/ASVTorch_Kaldi/score.txt 
- 
+
+CUDA_VISIBLE_DEVICES=0 python main_SSL_LA.py --track=LA --is_eval --eval --model_path=/app/model/LA_model.pth --eval_output=$resultsPath/eval_CM_scores_file_SSL_LA.txt --database_path /app/ --protocols_path shorteval/
 python evaluate_2021_LA.py /app/results/eval_CM_scores_file_SSL_LA.txt /app/ASVspoof2021_LA_eval/keys/LA/ progress
+
+#CUDA_VISIBLE_DEVICES=0 python main_SSL_DF.py --track=DF --is_eval --eval --model_path=/app/model/Best_LA_model_for_DF.pth --eval_output=$resultsPath/eval_CM_scores_file_SSL_Best_LA_model_for_DF.txt --database_path /app/ --protocols_path shorteval/
+#python evaluate_2021_DF.py /app/results/eval_CM_scores_file_SSL_Best_LA_model_for_DF.txt /app/ASVspoof2021_LA_eval/keys/LA/ progress
