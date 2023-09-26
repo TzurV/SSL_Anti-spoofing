2dfe0d9bf0b296cb3d26db123399248b8e130eb0
Tzur Vaich <tzurvaich@gmail.com>
2023-09-26 09:23:28 +0100

    add DF mapping

--- Docker_run.bat
+++ Docker_run.bat
@@ -7,5 +7,8 @@ REM            -it build1
 docker run -v "C:\work\Github\Tensor_Lihu\MultiKol\SSL_Anti-spoofing\results:/app/results" ^
            -v "C:\work\Github\Tensor_Lihu\MultiKol\SSL_Anti-spoofing\model:/app/model" ^
            -v "C:\work\Github\Tensor_Lihu\MultiKol\ASVspoof2021\ASVspoof2021_LA_eval:/app/ASVspoof2021_LA_eval" ^
+           -v "C:\work\Github\Tensor_Lihu\MultiKol\ASVspoof2021\ASVspoof2021_DF_eval_part00:/app/ASVspoof2021_DF_eval_part00" ^
            -it build1
+
+REM            -v "C:\work\Github\Tensor_Lihu\MultiKol\ASVspoof2021\ASVspoof2021_DF_eval_part01:/app/ASVspoof2021_DF_eval_part01"
            
