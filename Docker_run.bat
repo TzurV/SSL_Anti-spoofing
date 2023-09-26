caf0841c03b89f65595911448485a4476c71a36a
Tzur Vaich <tzurvaich@gmail.com>
2023-09-26 14:31:16 +0100

    add link to in the wild

--- Docker_run.bat
+++ Docker_run.bat
@@ -8,6 +8,7 @@ docker run -v "C:\work\Github\Tensor_Lihu\MultiKol\SSL_Anti-spoofing\results:/ap
            -v "C:\work\Github\Tensor_Lihu\MultiKol\SSL_Anti-spoofing\model:/app/model" ^
            -v "C:\work\Github\Tensor_Lihu\MultiKol\ASVspoof2021\ASVspoof2021_LA_eval:/app/ASVspoof2021_LA_eval" ^
            -v "C:\work\Github\Tensor_Lihu\MultiKol\ASVspoof2021_DF_eval_part00/ASVspoof2021_DF_eval:/app/ASVspoof2021_DF_eval" ^
+           -v "C:\work\Github\Tensor_Lihu\MultiKol\release_in_the_wild:/app/release_in_the_wild" ^
            -it build1
 
 REM            -v "C:\work\Github\Tensor_Lihu\MultiKol\ASVspoof2021\ASVspoof2021_DF_eval_part01:/app/ASVspoof2021_DF_eval_part01"
