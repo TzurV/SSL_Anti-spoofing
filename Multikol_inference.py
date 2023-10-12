ea42bea79ef7930314fd88b2d52420dca973208f
Tzur Vaich <tzurvaich@gmail.com>
2023-10-12 15:05:03 +0100

    return real_person

--- Multikol_inference.py
+++ Multikol_inference.py
@@ -52,15 +52,19 @@ class multikol_service:
         batch_out = self.model(batch_x)
         
         score = (batch_out[:, 1]).data.cpu().numpy().ravel() 
+        real_person = True
         if score < self.threshold:
             hypothesis = "spoof"
+            real_person = False
         else:
             hypothesis = "bonafide"
 
-        results = {"score": score[0], 
+        results = {"score": score[0],
+                   "real_person": real_person,
                    "hypothesis": hypothesis,
                    "original_length": audio_np.shape[0],
-                   "padded_length": X_pad.shape[0]}
+                   "padded_length": X_pad.shape[0],
+                   "threshold": self.threshold}
         logger.info(f"results: {results}")
         
         return results
